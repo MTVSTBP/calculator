68703b255b8cc7da717f23172344b11ac0f205a8
lsy1307 <lsy1307@inha.edu>
2024-06-18 16:50:54 +0900

    곱하기 기능 개발 완료 (#1)

--- src/test/java/com/tbp/calculator/MultipliorTests.java
+++ src/test/java/com/tbp/calculator/MultipliorTests.java
@@ -1,4 +1,33 @@
 package com.tbp.calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MultipliorTests {
+    @Test
+    @DisplayName("1과 2를 곱한 결과가 2인지 확인")
+    public void testMul1() {
+        //given
+        int a = 1;
+        int b = 2;
+
+        //when
+        int result = new Multiplior().mul(a,b);
+        //then (verify)
+        Assertions.assertEquals(2,result);
+    }
+
+    @Test
+    @DisplayName("1과 2를 곱한 결과가 2인지 확인")
+    public void testMul2() {
+        //given
+        int a = 2;
+        int b = 3;
+
+        //when
+        int result = new Multiplior().mul(a,b);
+        //then (verify)
+        Assertions.assertEquals(6,result);
+    }
 }
